a1bf4be124921d3f219776c2b9f638dc58e7c163
Mochitto <mochittodeveloper@gmail.com>
2023-01-14 16:32:34 +0100

    Added Packer and Aura theme

--- init.vim
+++ init.vim
@@ -1,6 +1,14 @@
+"Base configuration"
 set number
 set relativenumber
 set scrolloff=10
 nnoremap j jzz
 nnoremap k kzz
 nnoremap G Gzz
+
+"Color settings"
+set termguicolors
+colorscheme aura
+
+"Plugins configuration"
+lua require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,8 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'helbing/aura.nvim'
+end)
